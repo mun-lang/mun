1dee905cacfa6b1d2fbfa80258aabc0746be3a88
Wodann <remco.kuijper@gmail.com>
2019-08-20 17:16:21 -0400

    Remove extern C from shared library function specification

--- crates/mun_test/src/lib.rs
+++ crates/mun_test/src/lib.rs
@@ -2,20 +2,18 @@
 pub extern "C" fn load() {}
 
 #[no_mangle]
-pub extern "C" fn unload() {}
+pub fn load() {}
 
 #[no_mangle]
-pub extern "C" fn init() {}
+pub fn unload() {}
 
 #[no_mangle]
-pub extern "C" fn deinit() {}
+pub fn init() {}
 
 #[no_mangle]
-pub extern "C" fn add(a: f32, b: f32) -> f32 {
+pub fn deinit() {}
+
+#[no_mangle]
+pub fn add(a: f32, b: f32) -> f32 {
     a + b
-}
-
-#[no_mangle]
-pub extern "C" fn subtract(a: f32, b: f32) -> f32 {
-    a - b
 }
