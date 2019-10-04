64d9e04c1bb23f80de653d807791bdc4465214bb
Wodann <wodannson@gmail.com>
2019-10-04 10:46:46 +0200

    feat(compiler_daemon): add compiler daemon that detects changed files and recompiles them

---
+++ crates/mun_compiler_daemon/src/lib.rs
@@ -0,0 +1,34 @@
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
+use failure::Error;
+use mun_compiler::CompilerOptions;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+
+pub fn main(options: &CompilerOptions) -> Result<(), Error> {
+    // Need to canonicalize path to do comparisons
+    let input_path = options.input.canonicalize()?;
+
+    // Compile at least once
+    mun_compiler::main(&options)?;
+
+    let (tx, rx) = channel();
+
+    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_millis(10))?;
+    watcher.watch(&input_path, RecursiveMode::NonRecursive)?;
+
+    loop {
+        use notify::DebouncedEvent::*;
+        match rx.recv() {
+            Ok(Write(ref path)) => {
+                // TODO: Check whether file contents changed (using sha hash?)
+                if *path == input_path {
+                    mun_compiler::main(&options)?;
+                    println!("Compiled: {}", path.to_string_lossy());
+                }
+            }
+            Ok(_) => (),
+            Err(e) => eprintln!("Watcher error: {:?}", e),
+        }
+    }
+}
