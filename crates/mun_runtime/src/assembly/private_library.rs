c6541dc7e9aa64e77f8af80f9b733990fef3050a
Bas Zalmstra <zalmstra.bas@gmail.com>
2019-10-14 21:31:43 +0200

    refactor: use shared object copying everywhere

--- crates/mun_runtime/src/assembly/private_library.rs
+++ crates/mun_runtime/src/assembly/private_library.rs
@@ -2,30 +2,18 @@ use super::Library;
 use std::path::Path;
 use failure::Error;
 
-/// A structure that holds a `Library` instance but enables writing to the original library.
+/// A structure that holds a `Library` instance but creates a unique file per load this enables
+/// writing to the original library and ensures that each shared object on Linux is loaded
+/// separately.
 ///
-/// On Windows while a library is loaded it cannot be written to. On Windows this library copies the
-/// library to a temporary path and loads the library from there. When the `PrivateLibrary` is
-/// dropped, the internal library is unloaded and the temporary file is deleted.
-///
-/// There is no risk of Windows cleaning the temporary file while it is used because loading the
-/// library keeps the file open.
+/// There is no risk of cleaning the temporary file while it is used because loading the library
+/// keeps the file open (Windows) or keeping the file is not required in the first place (*nix).
 pub struct PrivateLibrary {
-    #[cfg(target_os = "windows")]
     tmp_path: tempfile::TempPath,
     library: Library
 }
 
 impl PrivateLibrary {
-    #[cfg(not(target_os = "windows"))]
-    pub fn new(path: &Path) -> Result<Self, Error> {
-        let library = Library::new(path)?;
-        Ok(PrivateLibrary {
-            library
-        })
-    }
-
-    #[cfg(target_os = "windows")]
     pub fn new(path: &Path) -> Result<Self, Error> {
         let tmp_path = tempfile::NamedTempFile::new()?.into_temp_path();
         let library = Library::new(&tmp_path)?;
