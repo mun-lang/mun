1b8ac4b5e9f65ae131dab91508b0bbcba59c57af
Wodann <wodannson@gmail.com>
2019-10-27 12:49:13 +0100

    feat(runtime_capi): add error reporting

---
+++ crates/mun_runtime_capi/src/hub.rs
@@ -0,0 +1,71 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::sync::Arc;
+
+use failure::Error;
+use lazy_static::lazy_static;
+use parking_lot::{RwLock, RwLockReadGuard};
+
+use crate::error::ErrorHandle;
+use crate::TypedHandle;
+
+fn generate_handle<H: TypedHandle>() -> H {
+    loop {
+        let token = rand::random();
+        if token != 0 {
+            return H::new(token);
+        }
+    }
+}
+
+pub struct Registry<H, T> {
+    data: RwLock<HashMap<H, T>>,
+}
+
+impl<H, T> Registry<H, T>
+where
+    H: Copy + Eq + Hash + TypedHandle,
+{
+    pub fn register(&self, value: T) -> H {
+        let handle = {
+            let data = self.data.read();
+
+            let mut handle = generate_handle();
+            while data.contains_key(&handle) {
+                handle = generate_handle();
+            }
+            handle
+        };
+
+        self.data.write().insert(handle, value);
+        handle
+    }
+
+    pub fn unregister(&self, handle: H) -> Option<T> {
+        self.data.write().remove(&handle)
+    }
+
+    pub fn get_data(&self) -> RwLockReadGuard<HashMap<H, T>> {
+        self.data.read()
+    }
+}
+
+impl<H, T> Default for Registry<H, T>
+where
+    H: Eq + Hash + TypedHandle,
+{
+    fn default() -> Self {
+        Registry {
+            data: RwLock::new(HashMap::new()),
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct Hub {
+    pub errors: Arc<Registry<ErrorHandle, Box<Error>>>,
+}
+
+lazy_static! {
+    pub static ref HUB: Hub = Hub::default();
+}
--- crates/mun_runtime_capi/src/lib.rs
+++ crates/mun_runtime_capi/src/lib.rs
@@ -1,72 +1,111 @@
-use mun_abi::FunctionInfo;
-use mun_runtime::{MunRuntime, RuntimeBuilder};
+mod error;
+mod hub;
+
 use std::ffi::{c_void, CStr};
 use std::os::raw::c_char;
+
+use crate::error::ErrorHandle;
+use crate::hub::HUB;
+use failure::err_msg;
+use mun_abi::FunctionInfo;
+use mun_runtime::{MunRuntime, RuntimeBuilder};
+
+pub(crate) type Token = usize;
+
+pub trait TypedHandle {
+    fn new(token: Token) -> Self;
+    fn token(&self) -> Token;
+}
 
 #[repr(C)]
 pub struct RuntimeHandle(*mut c_void);
 
 #[no_mangle]
-pub unsafe extern "C" fn create_runtime(
+pub unsafe extern "C" fn mun_runtime_create(
     library_path: *const c_char,
     handle: *mut RuntimeHandle,
-) -> u64 /* error */
-{
+) -> ErrorHandle {
     if library_path.is_null() {
-        return 1;
+        return HUB.errors.register(Box::new(err_msg(
+            "Invalid argument: 'library_path' is null pointer.",
+        )));
     }
 
     let library_path = match CStr::from_ptr(library_path).to_str() {
         Ok(path) => path,
-        Err(_) => return 2,
+        Err(_) => {
+            return HUB.errors.register(Box::new(err_msg(
+                "Invalid argument: 'library_path' is not UTF-8 encoded.",
+            )))
+        }
     };
 
     let handle = match handle.as_mut() {
         Some(handle) => handle,
-        None => return 3,
+        None => {
+            return HUB.errors.register(Box::new(err_msg(
+                "Invalid argument: 'handle' is null pointer.",
+            )))
+        }
     };
 
     let runtime = match RuntimeBuilder::new(library_path).spawn() {
         Ok(runtime) => runtime,
-        Err(_) => return 4,
+        Err(e) => return HUB.errors.register(Box::new(e)),
     };
 
     handle.0 = Box::into_raw(Box::new(runtime)) as *mut _;
-    0
+    ErrorHandle::default()
 }
 
 #[no_mangle]
-pub extern "C" fn destroy_runtime(handle: RuntimeHandle) {
+pub extern "C" fn mun_runtime_destroy(handle: RuntimeHandle) {
     if !handle.0.is_null() {
         let _runtime = unsafe { Box::from_raw(handle.0) };
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn runtime_get_function_info(
+pub unsafe extern "C" fn mun_runtime_get_function_info(
     handle: RuntimeHandle,
     fn_name: *const c_char,
     has_fn_info: *mut bool,
     fn_info: *mut FunctionInfo,
-) -> u64 /* error */ {
+) -> ErrorHandle {
     let runtime = match (handle.0 as *mut MunRuntime).as_ref() {
         Some(runtime) => runtime,
-        None => return 1,
+        None => {
+            return HUB.errors.register(Box::new(err_msg(
+                "Invalid argument: 'runtime' is null pointer.",
+            )))
+        }
     };
 
     let fn_name = match CStr::from_ptr(fn_name).to_str() {
         Ok(name) => name,
-        Err(_) => return 2,
+        Err(_) => {
+            return HUB.errors.register(Box::new(err_msg(
+                "Invalid argument: 'fn_name' is not UTF-8 encoded.",
+            )))
+        }
     };
 
     let has_fn_info = match has_fn_info.as_mut() {
         Some(has_info) => has_info,
-        None => return 3,
+        None => {
+            return HUB.errors.register(Box::new(err_msg(
+                "Invalid argument: 'has_fn_info' is null pointer.",
+            )))
+        }
     };
 
     let fn_info = match fn_info.as_mut() {
         Some(info) => info,
-        None => return 4,
+        None => {
+            return HUB.errors.register(Box::new(err_msg(
+                "Invalid argument: 'fn_info' is null pointer.",
+            )))
+        }
     };
 
     match runtime.get_function_info(fn_name) {
@@ -77,22 +116,32 @@ pub unsafe extern "C" fn runtime_get_function_info(
         None => *has_fn_info = false,
     }
 
-    0
+    ErrorHandle::default()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn runtime_update(handle: RuntimeHandle, updated: *mut bool) -> u64 /* error */
-{
+pub unsafe extern "C" fn mun_runtime_update(
+    handle: RuntimeHandle,
+    updated: *mut bool,
+) -> ErrorHandle {
     let runtime = match (handle.0 as *mut MunRuntime).as_mut() {
         Some(runtime) => runtime,
-        None => return 1,
+        None => {
+            return HUB.errors.register(Box::new(err_msg(
+                "Invalid argument: 'runtime' is null pointer.",
+            )))
+        }
     };
 
     let updated = match updated.as_mut() {
         Some(updated) => updated,
-        None => return 2,
+        None => {
+            return HUB.errors.register(Box::new(err_msg(
+                "Invalid argument: 'updated' is null pointer.",
+            )))
+        }
     };
 
     *updated = runtime.update();
-    0
+    ErrorHandle::default()
 }
