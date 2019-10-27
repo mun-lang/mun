4d8739bcb4d8dd83ba8bad067b6b53a3547ff412
Wodann <wodannson@gmail.com>
2019-10-27 12:44:05 +0100

    feat(runtime_capi): add automatic generation of C++ bindings for the runtime

--- crates/mun_runtime_capi/build.rs
+++ crates/mun_runtime_capi/build.rs
@@ -5,7 +5,7 @@ use cbindgen;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::generate(crate_dir)
-        .expect("Unable to generate Mun Runtime bindings.")
-        .write_to_file("c/include/mun_runtime.h");
+    let bindings = cbindgen::generate(crate_dir).expect("Unable to generate Mun Runtime bindings.");
+    bindings.write_to_file("c/include/mun/runtime.h");
+    bindings.write_to_file("cpp/include/mun/runtime_capi.h");
 }
