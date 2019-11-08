b139b48f58041aa4ded9f649b7ca5d5eadc1788f
Bas Zalmstra <zalmstra.bas@gmail.com>
2019-11-08 17:47:56 +0100

    test(hir): checks if source computations are properly cached

---
+++ crates/mun_hir/src/tests.rs
@@ -0,0 +1,49 @@
+use crate::db::HirDatabase;
+use crate::db::SourceDatabase;
+use crate::mock::MockDatabase;
+use std::sync::Arc;
+
+/// This function tests that the ModuleData of a module does not change if the contents of a function
+/// is changed.
+#[test]
+fn check_module_data_does_not_change() {
+    let (mut db, file_id) = MockDatabase::with_single_file(
+        r#"
+    fn foo():int {
+        1+1
+    }
+    "#,
+    );
+
+    {
+        let events = db.log_executed(|| {
+            db.module_data(file_id);
+        });
+        assert!(
+            format!("{:?}", events).contains("module_data"),
+            "{:#?}",
+            events
+        )
+    }
+    db.set_file_text(
+        file_id,
+        Arc::new(
+            r#"
+    fn foo():int {
+        90
+    }
+    "#
+            .to_owned(),
+        ),
+    );
+    {
+        let events = db.log_executed(|| {
+            db.module_data(file_id);
+        });
+        assert!(
+            !format!("{:?}", events).contains("module_data"),
+            "{:#?}",
+            events
+        )
+    }
+}
