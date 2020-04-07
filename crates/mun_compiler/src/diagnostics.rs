a6d865933578475c77ad446c5b54af119e318367
legendiguess <legendiguess@mail.ru>
2020-04-08 02:01:18 +0500

    test(diagnostic): add tests for diagnostic errors

--- crates/mun_compiler/src/diagnostics.rs
+++ crates/mun_compiler/src/diagnostics.rs
@@ -309,10 +309,86 @@ pub fn diagnostics(db: &impl HirDatabase, file_id: FileId) -> Vec<Snippet> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::{Color, Config, Driver, PathOrInline, RelativePathBuf};
 
     #[test]
     fn test_text_range_to_tuple() {
         let text_range = TextRange::from_to(3.into(), 5.into());
         assert_eq!(text_range_to_tuple(text_range), (3, 5));
+    }
+
+    /// Compile passed source code and return all compilation errors
+    fn compilation_errors(source_code: &str) -> String {
+        let config = Config {
+            color: Color::Disable,
+            ..Config::default()
+        };
+
+        let input = PathOrInline::Inline {
+            rel_path: RelativePathBuf::from("main.mun"),
+            contents: source_code.to_owned(),
+        };
+
+        let (driver, _) = Driver::with_file(config, input).unwrap();
+
+        let mut compilation_errors = Vec::<u8>::new();
+
+        let _ = driver.emit_diagnostics(&mut compilation_errors).unwrap();
+
+        String::from_utf8(compilation_errors).unwrap()
+    }
+
+    #[test]
+    fn test_syntax_error() {
+        insta::assert_display_snapshot!(compilation_errors("\n\nfn main(\n struct Foo\n"));
+    }
+
+    #[test]
+    fn test_unresolved_value_error() {
+        insta::assert_display_snapshot!(compilation_errors(
+            "\n\nfn main() {\nlet b = a;\n\nlet d = c;\n}"
+        ));
+    }
+
+    #[test]
+    fn test_unresolved_type_error() {
+        insta::assert_display_snapshot!(compilation_errors(
+            "\n\nfn main() {\nlet a = Foo{};\n\nlet b = Bar{};\n}"
+        ));
+    }
+
+    #[test]
+    fn test_expected_function_error() {
+        insta::assert_display_snapshot!(compilation_errors(
+            "\n\nfn main() {\nlet a = Foo();\n\nlet b = Bar();\n}"
+        ));
+    }
+
+    #[test]
+    fn test_mismatched_type_error() {
+        insta::assert_display_snapshot!(compilation_errors(
+            "\n\nfn main() {\nlet a: float = false;\n\nlet b: bool = 22;\n}"
+        ));
+    }
+
+    #[test]
+    fn test_duplicate_definition_error() {
+        insta::assert_display_snapshot!(compilation_errors(
+            "\n\nfn foo(){}\n\nfn foo(){}\n\nstruct Bar;\n\nstruct Bar;\n\nfn BAZ(){}\n\nstruct BAZ;"
+        ));
+    }
+
+    #[test]
+    fn test_possibly_uninitialized_variable_error() {
+        insta::assert_display_snapshot!(compilation_errors(
+            "\n\nfn main() {\nlet a;\nif 5>6 {\na = 5\n}\nlet b = a;\n}"
+        ));
+    }
+
+    #[test]
+    fn test_access_unknown_field_error() {
+        insta::assert_display_snapshot!(compilation_errors(
+            "\n\nstruct Foo {\ni: bool\n}\n\nfn main() {\nlet a = Foo { i: false };\nlet b = a.t;\n}"
+        ));
     }
 }
