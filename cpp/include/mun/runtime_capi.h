f255f50f43c04c1df66d447e652fc0c26c277257
Bas Zalmstra <zalmstra.bas@gmail.com>
2022-05-16 21:35:03 +0200

    fix: regenerated cbindgen files

--- cpp/include/mun/runtime_capi.h
+++ cpp/include/mun/runtime_capi.h
@@ -1,7 +1,7 @@
 #ifndef MUN_RUNTIME_BINDINGS_H_
 #define MUN_RUNTIME_BINDINGS_H_
 
-/* Generated with cbindgen:0.16.0 */
+/* Generated with cbindgen:0.23.0 */
 
 #include <stdbool.h>
 #include <stdint.h>
