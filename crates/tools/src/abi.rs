b22b281d6a1c892155b2bb1a0a7f94aa6ec83877
Bas Zalmstra <zalmstra.bas@gmail.com>
2022-05-28 18:17:23 +0200

    refactor: remove abi submodule

--- crates/tools/src/abi.rs
+++ crates/tools/src/abi.rs
@@ -5,7 +5,7 @@ pub const ABI_DIR: &str = "crates/mun_abi";
 /// Generates the FFI bindings for the Mun ABI
 pub fn generate(mode: Mode) -> Result<()> {
     let crate_dir = project_root().join(ABI_DIR);
-    let file_path = crate_dir.join("ffi/include/mun_abi.h");
+    let file_path = project_root().join("c/include/mun/abi.h");
 
     let mut file_contents = Vec::<u8>::new();
     cbindgen::generate(crate_dir)?.write(&mut file_contents);
