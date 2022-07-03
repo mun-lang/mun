40d1faf075b909bc93bdd5ba5ff1cf6868bf47dd
Wodann <Wodann@users.noreply.github.com>
2022-07-03 10:19:38 -0500

    misc: reorganise files

--- crates/mun_runtime/examples/bevy_simple.rs
+++ crates/mun_runtime/examples/bevy_simple.rs
@@ -1,13 +1,21 @@
+use std::{env, path::PathBuf};
+
 use bevy::prelude::*;
 use mun_runtime::Runtime as MunRuntime;
 
-const MUNLIB_PATH: &str = "mun/target/mod.munlib";
+// How to run?
+// 1. On the CLI, navigate to the `examples/bevy_simple` directory.
+// 2. Run the compiler daemon from the CLI: `/path/to/mun build --watch`
+// 3. Run the application from the CLI: cargo run --example bevy_simple -- target/mod.munlib
 
-// Minimal Bevy application.
+// Minimal Bevy application that demonstrates how to insert the Mun runtime into a Bevy world object and
+// utilizes the Mun runtime inside of Bevy systems.
 fn main() {
+    let lib_dir = PathBuf::from(env::args().nth(1).expect("Expected path to a Mun library."));
+
     App::new()
         .add_plugins(MinimalPlugins)
-        .add_plugin(MunPlugin)
+        .add_plugin(MunPlugin { lib_dir })
         .run();
 }
 
@@ -15,7 +23,9 @@ fn main() {
 // This example will build the core Mun functionality into a MunPlugin. This plugin is loaded into
 // Bevy in the "main()" function above. Through this plugin we will load and access the various
 // Mun pieces with Bevy.
-struct MunPlugin;
+struct MunPlugin {
+    lib_dir: PathBuf,
+}
 
 impl Plugin for MunPlugin {
     fn build(&self, app: &mut App) {
@@ -27,7 +37,8 @@ impl Plugin for MunPlugin {
         // We are using "exclusive_system()" to load the initial setup that inserts the Mun
         // runtime as a Bevy world resource, as well as other resources.
         // "exclusive_system()" is only necessary for loading the Mun runtime.
-        app.add_startup_system(setup.exclusive_system())
+        app.insert_resource(self.lib_dir.clone())
+            .add_startup_system(setup.exclusive_system())
             .add_system(print_from_mun)
             .add_system(reload_munlib_every_frame);
     }
@@ -37,13 +48,19 @@ struct PrintTimer(Timer);
 
 // Insert the Mun runtime, and associated timers as Bevy world resources.
 fn setup(world: &mut World) {
-    let builder = MunRuntime::builder(MUNLIB_PATH);
+    let builder = MunRuntime::builder(
+        world
+            .get_resource::<PathBuf>()
+            .expect("Lib path must be added as resource"),
+    );
     // We assume the Mun runtime is safe.
     let mun: MunRuntime = unsafe { builder.finish() }.expect("Failed to spawn Runtime");
     // Mun does not implement the send/sync trait so it needs to be inserted into Bevy as a
     // "non_send_resource".
     world.insert_non_send_resource(mun);
     world.insert_resource(PrintTimer(Timer::from_seconds(1.0, true)));
+
+    world.remove_resource::<PathBuf>();
 }
 
 fn reload_munlib_every_frame(
