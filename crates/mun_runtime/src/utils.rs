ef94d4bf1f0de64ada9f17faa1657fb7eb421ec1
Isabela Case <igoes@Isabelas-MacBook-Air.local>
2023-05-31 22:14:37 -0500

    docs: add documentation to function lev_distance

--- crates/mun_runtime/src/utils.rs
+++ crates/mun_runtime/src/utils.rs
@@ -1,5 +1,8 @@
 use std::cmp;
 
+/// The Levenshtein distance is a string metric for measuring the difference between two sequences
+/// A distance between two words is the minimum number of single-character edits
+/// (insertions, deletions or substitutions) required to change one word into the other
 pub fn lev_distance(a: &str, b: &str) -> usize {
     // cases which don't require further computation
     if a.is_empty() {
