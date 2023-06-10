4c4df401a993fc2d1dc7fa9c5723cf51be578b9f
Wodann <Wodann@users.noreply.github.com>
2023-06-10 00:41:16 -0700

    fix(hir): missing validators in test diagnostics (#527)

--- crates/mun_hir/src/utils.rs
+++ crates/mun_hir/src/utils.rs
@@ -12,12 +12,7 @@ pub(crate) fn make_mut_slice<T: Clone>(a: &mut Arc<[T]>) -> &mut [T] {
 #[cfg(test)]
 pub mod tests {
     use crate::{
-        code_model::r#struct::validator::StructValidator,
-        diagnostics::DiagnosticSink,
-        expr::validator::{ExprValidator, TypeAliasValidator},
-        mock::MockDatabase,
-        with_fixture::WithFixture,
-        FileId, ModuleDef, Package,
+        diagnostics::DiagnosticSink, mock::MockDatabase, with_fixture::WithFixture, Package,
     };
 
     pub fn diagnostics(content: &str) -> String {
@@ -29,24 +24,8 @@ pub mod tests {
             diags.push(format!("{:?}: {}", diag.highlight_range(), diag.message()));
         });
 
-        for item in Package::all(&db)
-            .iter()
-            .flat_map(|pkg| pkg.modules(&db))
-            .flat_map(|module| module.declarations(&db))
-        {
-            match item {
-                ModuleDef::Function(item) => {
-                    ExprValidator::new(item, &db).validate_body(&mut diag_sink);
-                }
-                ModuleDef::TypeAlias(item) => {
-                    TypeAliasValidator::new(item, &db)
-                        .validate_target_type_existence(&mut diag_sink);
-                }
-                ModuleDef::Struct(item) => {
-                    StructValidator::new(item, &db, FileId(0)).validate_privacy(&mut diag_sink);
-                }
-                _ => {}
-            }
+        for module in Package::all(&db).iter().flat_map(|pkg| pkg.modules(&db)) {
+            module.diagnostics(&db, &mut diag_sink);
         }
 
         drop(diag_sink);
